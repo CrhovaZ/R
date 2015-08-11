ec6518cb485793d31b37c664cd61df941aec265a
CrhovaZ <zuzulka.horvathova@seznam.cz>
2015-08-11 18:18:22 +0200

    last

--- best_1.R
+++ best_1.R
@@ -1,14 +1,14 @@
-## Úkolem je napsat funkci, která vybere nejlepší nemocnici v zadaném státì 
-## podle nejnižší úmrtnosti pacientù na zadanou komplikaci.
+ï»¿## Û«olem je napsat funkci, která¡¶ybere nejlepÛ­ nemocnici v zadanê­ stâµ¬ 
+## podle nejniÞšð®¡­rtnosti pacientñ ®¡ zadanou komplikaci.
 best <- function(state, outcome) {
 	## Read outcome data
 # data <- read.csv ("D:/user/Desktop/Programming Assignment 3/outcome-of-care-measures.csv")
 data <- read.csv ("outcome-of-care-measures.csv")
-	## Pøejmenovala jsem potøebné sloupce, aby se s nimi jednodušeji pracovalo..
+	## Pð¥ª­enovala jsem potð¥¢®é¡³loupce, aby se s nimi jednoduÛ¥ji pracovalo..
 	colnames(data)[11] <- "heart attack"  # blank space might cause problems
 	colnames(data)[17] <- "heart failure" # the same problem
 	colnames(data)[23] <- "pneumonia"
-	## Vytvoøila jsem subset, obsahuje data zadaného státu
+	## Vytvoð©¬¡ jsem subset, obsahuje data zadanê©¯ stâµµ
 
 	if (!is.element(state, data$State)){
 	 stop("invalid state")
@@ -20,8 +20,8 @@ data <- read.csv ("outcome-of-care-measures.csv")
 	}
 
 	subset.state <- subset(data, State==state, c(outcome, "Hospital.Name") ) ## I can subset certain column but I need to replace it with "outcome"
-	## Tady mi to pøestává fungovat... Využila jsem rady z diskuzního fóra a zkusila pøevést data 
-	## na numerická a následnì je seøadit, ale nefunguje to...
+	## Tady mi to pð¥³´â·¡ fungovat... Vyuß©la jsem rady z diskuznî©¯ fó²¡ a zkusila pð¥¶©st data 
+	## na numerická¡¡ nâ´¬ednì¡ªe seð¡¤©t, ale nefunguje to...
  numeric.data <- suppressWarnings( data.frame(
  outcome = as.numeric(as.character(subset.state[ ,1])), 
  Hospital.Name = subset.state$Hospital.Name ) )
